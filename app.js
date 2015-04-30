aea7fa243996762151828baa15c038316321368c
k.kalchenko <k.kalchenko@corp.mail.ru>
2015-04-30 18:47:16 +0300

    fix

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var app = express();
 var http = require('http');
 
-app.set('port', (process.env.PORT || 3000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function(){
 	console.log('Express server');
@@ -17,17 +17,17 @@ app.get('/bids', function(req, res) {
 		i=0,
 		queryString = "";
 		for(;i<pairs.length;i++){
-			queryString+=pairs[i]
-			queryString+=','
+			queryString+=pairs[i];
+			queryString+=',';
 		}
 		//queryString = queryString.slice(0,-1);
 
-		url = "http://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22"+queryString+"%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+		var url = "http://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22"+queryString+"%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 
 		http.get(url,function(response){
 			response.setEncoding('utf8');
 			response.on('data', function(chunk){
-				results = JSON.parse(chunk).query.results.rate;
+				var results = JSON.parse(chunk).query.results.rate;
 				console.log(results);
 				for(i=0;i<results.length-1;i++){
 					newPairs.push({
@@ -39,7 +39,7 @@ app.get('/bids', function(req, res) {
 				res.setHeader('Content-Type', 'application/json');
 				res.send(newPairs);
 			});		
-		})
+		});
 	}else{
 		res.setHeader('Content-Type', 'application/json');
 		res.send(newPairs);
